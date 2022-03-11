6da26ccacab89fbbc84c33108a7624306b1800e4
yangys <1095737364@qq.com>
2022-03-11 21:43:28 +0800

    首次提交

---
+++ szcpvue/src/store/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import user from './module/user'
+import app from './module/app'
+import sysicon from './module/sysicon'
+import sysmenu from './module/sysmenu'
+import sysorgan from './module/sysorgan'
+import sysrole from './module/sysrole'
+// import sysmarkdown from './module/sysmarkdown'
+import sysarticlemenu  from './module/sysarticlemenu'
+import sysfundtype  from './module/sysfundtype'
+import sysfundinfo  from './module/sysfundinfo'
+import sysarticle  from './module/sysarticle'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+
+  state: {
+  
+  },
+  mutations: {
+    
+  },
+  actions: {
+    //
+  },
+  modules: {
+    user,
+    app,
+    sysicon,
+    sysmenu,
+    sysorgan,
+    sysrole,
+    // sysmarkdown,
+    sysarticlemenu,
+    sysfundtype,
+    sysfundinfo,
+    sysarticle
+  }
+})
